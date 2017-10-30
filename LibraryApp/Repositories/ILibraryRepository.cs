1892bc60caa26b447aa4cb1820fc20fea9c1ad0b
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-10-30 22:51:07 +0000

    adding functions for repo actions

--- LibraryApp/Repositories/ILibraryRepository.cs
+++ LibraryApp/Repositories/ILibraryRepository.cs
@@ -1,18 +1,78 @@
 using System.Collections.Generic;
 using LibraryApp.Models.DTOModels;
+using LibraryApp.Models.ViewModels;
 
 namespace LibraryApp.Repositories
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface ILibraryRepository
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
          IEnumerable<BookDTO> GetAllBooks();
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="bookId"></param>
+         /// <returns></returns>
          BookDetailsDTO GetBookById(int bookId);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <returns></returns>
          IEnumerable<UserDTO> GetAllUsers();
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="userId"></param>
+         /// <returns></returns>
          UserDetailsDTO GetUserById(int userId);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="bookId"></param>
+         /// <returns></returns>
          IEnumerable<BookLiteDTO> GetBooksUserHasInLoan(int bookId);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="userId"></param>
+         /// <returns></returns>
          IEnumerable<ReviewDTO> GetReviewsUserHasMade(int userId);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="userId"></param>
+         /// <param name="bookId"></param>
+         /// <returns></returns>
          ReviewDetailsDTO GetReviewDetails(int userId, int bookId);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <param name="bookId"></param>
+         /// <returns></returns>
          IEnumerable<ReviewDTO> GetAllReviewsOfABook(int bookId);
+         /// <summary>
+         /// 
+         /// </summary>
+         /// <returns></returns>
          IEnumerable<ReviewDTO> GetAllReviews();
+         BookDetailsDTO AddNewBook(BookViewModel newBook);
+         BookDetailsDTO UpdateBook(int bookId, BookViewModel updatedBook);
+         UserDetailsDTO AddNewUser(UserViewModel newUser);
+         UserDetailsDTO UpdateUserInfo(int userId, UserViewModel updatedUser);
+         OutloanDTO AddNewLoan(int userId, int bookId);
+         OutloanDetailsDTO ReturnBook(int userId, int bookId);
+         OutloanDetailsDTO UpdateLoan(int userId, int bookId, LoanViewModel updateLoan);
+         ReviewDTO AddNewReview(int bookId, int userId, ReviewViewModel newReview);
+         ReviewDetailsDTO UpdateBookReview(int userId, int bookId, ReviewViewModel updatedReview);
+         bool RemoveBookFromLibrary(int bookId);
+         bool RemoveBookReview(int bookId, int userId);
+         bool RemoveUser(int userId);
+         bool AdminRemoveUserFromDatabase(int userId);
     }
 }
--- LibraryApp/Repositories/LibraryRepository.cs
+++ LibraryApp/Repositories/LibraryRepository.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using LibraryApp.Models.DTOModels;
+using LibraryApp.Models.ViewModels;
 
 namespace LibraryApp.Repositories
 {
     public class LibraryRepository : ILibraryRepository
     {
+        public BookDetailsDTO AddNewBook(BookViewModel newBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutloanDTO AddNewLoan(int userId, int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReviewDTO AddNewReview(int bookId, int userId, ReviewViewModel newReview)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserDetailsDTO AddNewUser(UserViewModel newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AdminRemoveUserFromDatabase(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<BookDTO> GetAllBooks()
         {
             throw new NotImplementedException();
@@ -47,6 +73,46 @@ namespace LibraryApp.Repositories
         }
 
         public UserDetailsDTO GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveBookFromLibrary(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveBookReview(int bookId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutloanDetailsDTO ReturnBook(int userId, int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookDetailsDTO UpdateBook(int bookId, BookViewModel updatedBook)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReviewDetailsDTO UpdateBookReview(int userId, int bookId, ReviewViewModel updatedReview)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutloanDetailsDTO UpdateLoan(int userId, int bookId, LoanViewModel updateLoan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserDetailsDTO UpdateUserInfo(int userId, UserViewModel updatedUser)
         {
             throw new NotImplementedException();
         }
