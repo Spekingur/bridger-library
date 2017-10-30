ecadf33cc17b1a1c404225cbcb0c4f0c9bde7559
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-10-30 19:43:19 +0000

    adding interface for Library actions, most GET actions now in interface

--- LibraryApp/Repositories/ILibraryRepository.cs
+++ LibraryApp/Repositories/ILibraryRepository.cs
@@ -1,9 +1,18 @@
 using System.Collections.Generic;
+using LibraryApp.Models.DTOModels;
 
 namespace LibraryApp.Repositories
 {
     public interface ILibraryRepository
     {
-         
+         IEnumerable<BookDTO> GetAllBooks();
+         BookDetailsDTO GetBookById(int bookId);
+         IEnumerable<UserDTO> GetAllUsers();
+         UserDetailsDTO GetUserById(int userId);
+         IEnumerable<BookLiteDTO> GetBooksUserHasInLoan(int bookId);
+         IEnumerable<ReviewDTO> GetReviewsUserHasMade(int userId);
+         ReviewDetailsDTO GetReviewDetails(int userId, int bookId);
+         IEnumerable<ReviewDTO> GetAllReviewsOfABook(int bookId);
+         IEnumerable<ReviewDTO> GetAllReviews();
     }
 }
--- LibraryApp/Repositories/LibraryRepository.cs
+++ LibraryApp/Repositories/LibraryRepository.cs
@@ -1,8 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using LibraryApp.Models.DTOModels;
 
 namespace LibraryApp.Repositories
 {
-    public class LibraryRepository
+    public class LibraryRepository : ILibraryRepository
     {
+        public IEnumerable<BookDTO> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ReviewDTO> GetAllReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ReviewDTO> GetAllReviewsOfABook(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserDTO> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookDetailsDTO GetBookById(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookLiteDTO> GetBooksUserHasInLoan(int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReviewDetailsDTO GetReviewDetails(int userId, int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ReviewDTO> GetReviewsUserHasMade(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserDetailsDTO GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
