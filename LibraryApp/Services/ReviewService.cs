73d98da35c79f3b8b39c625d35f082188d2d53ee
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-10-31 23:44:40 +0000

    added functions for review services

--- LibraryApp/Services/ReviewService.cs
+++ LibraryApp/Services/ReviewService.cs
@@ -1,44 +1,59 @@
 using System.Collections.Generic;
 using LibraryApp.Models.DTOModels;
 using LibraryApp.Models.ViewModels;
+using LibraryApp.Repositories;
 
 namespace LibraryApp.Services
 {
     public class ReviewService : IReviewService
     {
+        private IReviewRepository _repo;
+
+        public ReviewService(IReviewRepository repo)
+        {
+            _repo = repo;
+        }
+
         public ReviewDTO AddNewReview(int bookId, int userId, ReviewViewModel newReview)
         {
-            throw new System.NotImplementedException();
+            var review = _repo.AddNewReview(bookId, userId, newReview);
+            return review;
         }
 
         public IEnumerable<ReviewDTO> GetAllReviews()
         {
-            throw new System.NotImplementedException();
+            var reviews = _repo.GetAllReviews();
+            return reviews;
         }
 
         public IEnumerable<ReviewDTO> GetAllReviewsOfABook(int bookId)
         {
-            throw new System.NotImplementedException();
+            var reviews = _repo.GetAllReviewsOfABook(bookId);
+            return reviews;
         }
 
         public ReviewDetailsDTO GetReviewDetails(int userId, int bookId)
         {
-            throw new System.NotImplementedException();
+            var review = _repo.GetReviewDetails(userId, bookId);
+            return review;
         }
 
         public IEnumerable<ReviewDTO> GetReviewsUserHasMade(int userId)
         {
-            throw new System.NotImplementedException();
+            var reviews = _repo.GetReviewsUserHasMade(userId);
+            return reviews;
         }
 
         public bool RemoveBookReview(int bookId, int userId)
         {
-            throw new System.NotImplementedException();
+            var removeReview = _repo.RemoveBookReview(bookId, userId);
+            return removeReview;
         }
 
         public ReviewDetailsDTO UpdateBookReview(int userId, int bookId, ReviewViewModel updatedReview)
         {
-            throw new System.NotImplementedException();
+            var updateReview = _repo.UpdateBookReview(userId, bookId, updatedReview);
+            return updatedReview;
         }
     }
 }
