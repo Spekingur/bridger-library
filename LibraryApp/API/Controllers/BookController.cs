6374128fdce040df2357606be2ffb3f785de2bc4
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-10-31 21:54:05 +0000

    updated code in controllers

---
+++ LibraryApp/API/Controllers/BookController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryApp.Models.ViewModels;
+using LibraryApp.Services;
+
+namespace API.Controllers
+{
+    [Route("books")]
+    public class BookController : Controller
+    {
+        private IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        // GET /books
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetBooks()
+        {
+            var books = _bookService.GetAllBooks();
+            return Ok(books);
+        }
+
+        // GET /books/5
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _bookService.GetBookById(id);
+
+            if(book == null) { return NotFound(); }
+
+            return Ok(book);
+        }
+
+        // POST /books
+        [HttpPost]
+        [Route("", Name = "AddNewBook")]
+        public IActionResult AddNewBook([FromBody]BookViewModel newBook)
+        {
+            if(newBook == null) { return BadRequest(); }
+            if(!ModelState.IsValid) { return StatusCode(412); }
+
+            var user = _bookService.AddNewBook(newBook);
+
+            //return Ok(user);
+            return Created($"/books/newBook.Id", newBook);
+        }
+
+        // PUT /books/5
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody]BookViewModel updatedBook)
+        {
+            if(updatedBook == null) { return BadRequest(); }
+            if(!ModelState.IsValid) { return StatusCode(412); }
+
+            var book = _bookService.UpdateBookInfo(id, updatedBook);
+
+            if(book == null) { return NotFound(); }
+
+            return Ok(book);
+        }
+
+        // DELETE /books/5
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult RemoveBook(int id)
+        {
+            var success = _bookService.RemoveBook(id);
+            
+            if(!success) { return NotFound(); }
+            
+            return NoContent();
+        }
+    }
+}
---
+++ LibraryApp/API/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryApp.Models.ViewModels;
+using LibraryApp.Services;
+
+namespace API.Controllers
+{
+    [Route("users")]
+    public class UserController : Controller
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET /users
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetUsers()
+        {
+            var users = _userService.GetAllActiveUsers();
+            return Ok(users);
+        }
+
+        // GET /users/5
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        // POST /users
+        [HttpPost]
+        [Route("", Name = "AddNewUser")]
+        public IActionResult AddNewUser([FromBody]UserViewModel newUser)
+        {
+            if(newUser == null) { return BadRequest(); }
+            if(!ModelState.IsValid) { return StatusCode(412); }
+
+            var user = _userService.AddNewUser(newUser);
+
+            return Ok(user);
+        }
+
+        // PUT /users/5
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateUserInfo(int id, [FromBody]UserViewModel updatedUser)
+        {
+            if(updatedUser == null) { return BadRequest(); }
+            if(!ModelState.IsValid) { return StatusCode(412); }
+
+            var user = _userService.UpdateUserInfo(id, updatedUser);
+
+            if(user == null) { return NotFound(); }
+
+            return Ok(user);
+        }
+
+        // DELETE /users/5
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult RemoveUser(int id)
+        {
+            var success = _userService.RemoveUser(id);
+
+            if(!success) { return NotFound(); }
+
+            return NoContent();
+        }
+    }
+}
