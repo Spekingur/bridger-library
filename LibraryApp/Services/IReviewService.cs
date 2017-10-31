19e6a4990e54181282f96f84515c702d726b87fa
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-10-31 23:06:29 +0000

    adding basics for review services

--- LibraryApp/Services/IReviewService.cs
+++ LibraryApp/Services/IReviewService.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
+using LibraryApp.Models.DTOModels;
+using LibraryApp.Models.ViewModels;
+
 namespace LibraryApp.Services
 {
     public interface IReviewService
     {
-         
+        IEnumerable<ReviewDTO> GetReviewsUserHasMade(int userId);
+
+        ReviewDetailsDTO GetReviewDetails(int userId, int bookId);
+
+        IEnumerable<ReviewDTO> GetAllReviews();
+
+        IEnumerable<ReviewDTO> GetAllReviewsOfABook(int bookId);
+
+        ReviewDTO AddNewReview(int bookId, int userId, ReviewViewModel newReview);
+
+        ReviewDetailsDTO UpdateBookReview(int userId, int bookId, ReviewViewModel updatedReview);
+
+        bool RemoveBookReview(int bookId, int userId);
     }
 }
--- LibraryApp/Services/ReviewService.cs
+++ LibraryApp/Services/ReviewService.cs
@@ -1,7 +1,44 @@
+using System.Collections.Generic;
+using LibraryApp.Models.DTOModels;
+using LibraryApp.Models.ViewModels;
+
 namespace LibraryApp.Services
 {
     public class ReviewService : IReviewService
     {
-        
+        public ReviewDTO AddNewReview(int bookId, int userId, ReviewViewModel newReview)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ReviewDTO> GetAllReviews()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ReviewDTO> GetAllReviewsOfABook(int bookId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ReviewDetailsDTO GetReviewDetails(int userId, int bookId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ReviewDTO> GetReviewsUserHasMade(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveBookReview(int bookId, int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ReviewDetailsDTO UpdateBookReview(int userId, int bookId, ReviewViewModel updatedReview)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
